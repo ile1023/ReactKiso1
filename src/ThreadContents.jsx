311afc0b5a406a197b1d54ae732a6805afeb898b
ile1023 <binpuyoucai@gmail.com>
2024-11-09 19:04:51 +0900

    スレッドに対する投稿の一覧画面を作成

--- src/ThreadContents.jsx
+++ src/ThreadContents.jsx
@@ -1,16 +1,39 @@
-import './ThreadContent.css'
-import { useParams } from 'react-router-dom'
+import './ThreadContent.css';
+import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 
 function ThreadContents(){ 
-    const {thread_id} = useParams();
-    const detailData = async () => {
-        const response = await fetch('https://railway.bulletinboard.techtrain.dev/threads');
-        const data = response.json();
-    }
+    const {threadId} = useParams();
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const detailData = async () => {
+            try {
+                const response = await fetch(`https://railway.bulletinboard.techtrain.dev/threads/${threadId}/posts`);
+                const data = await response.json();
+                setPosts(data.posts);
+                setLoading(false);
+            } catch (error) {
+                console.error('エラーです');
+                setLoading(false);
+            }
+        };
+        detailData();
+    }, [threadId]);
+
+    if (loading) return <p>読み込み中...</p>;
     return(
-        <div>
-            <p>{thread_id}</p>
-           
+        <div className='ThreadContents'>
+            <p>投稿の詳細</p>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
